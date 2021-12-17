e7758dba9dbb15bb555815a3ea0695966f753e46
purnima <devipurnima992@gmail.com>
2021-12-17 05:49:57 +0000

    validation changes

--- src/com/lt/crs/validation/LoginValidation.java
+++ src/com/lt/crs/validation/LoginValidation.java
@@ -1,8 +1,48 @@
 package com.lt.crs.validation;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.lt.crs.utils.DbConnection;
+import com.mysql.jdbc.Connection;
 
 public class LoginValidation {
-	
+	DbConnection dbConn = new DbConnection();
 	public String validateCredentials(String userName, String password) {
-		return "Student";
+		int roleId = 0 ;
+		String role = null;
+		Connection conn=null;
+		PreparedStatement stmt=null ;
+		ResultSet rs = null;
+		PreparedStatement stmt1=null ;
+		ResultSet rs1 = null;
+		conn=(Connection) dbConn.createConnection();
+		String sql="select * from user";
+		
+		//System.out.println(sql);
+		try {
+			stmt= conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				if(userName.equalsIgnoreCase(rs.getString(2)) && password.equalsIgnoreCase(rs.getString(3))) {
+					roleId = rs.getInt(4);
+					String sql1="select role from role where id ="+roleId;
+					stmt1= conn.prepareStatement(sql1);
+					rs1 = stmt1.executeQuery();
+					while(rs1.next()) {
+					 role=rs1.getString(1);
+					}
+						
+				}
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//stmt.setInt(1, course.getCourseId()); // This would set age
+		dbConn.closeConnection(conn);
+		return role;
 	}
 }
+
+
